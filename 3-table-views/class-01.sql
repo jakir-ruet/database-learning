53d496bd8c9bc82d442cece52d00671317ea6785
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-12-13 14:16:16 +0600

    initial commit

---
+++ 3-table-views/class-01.sql
@@ -0,0 +1,16 @@
+-- create database mydatabase
+-- use mydatabase
+
+-- create table mytable
+-- (
+-- 	Id int unsigned,
+--     Name varchar(125),
+--     Father_Name varchar (125),
+--     Mother_Name varchar (125),
+--     Email varchar(120),
+--     Phone varchar(120)
+-- )
+
+-- select * from mytable
+
+-- insert into mytable(Id, Name, Father_Name, Mother_Name, Email, Phone) values(1, 'jakir', 'humaion', 'banu', 'jakir@gmail.com', '01788916805')
